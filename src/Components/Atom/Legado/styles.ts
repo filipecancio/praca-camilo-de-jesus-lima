051546c74e362ec7ed5363595ee13606f0d129b0
Filipe Câncio <filipe.cancio@outlook.com>
2021-06-06 14:48:45 -0300

    🚧 add styled components and add components

---
+++ src/Components/Atom/Legado/styles.ts
@@ -0,0 +1,37 @@
+
+import styled from "styled-components"
+
+export const Container = styled.div`
+    text-align: left;
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: white;
+`
+
+export const Content = styled.section`
+  max-width: 1000px;
+
+    a {
+    text-decoration: none;
+    color: #61dafb;
+  }
+`
+
+export const Map = styled.iframe`
+    margin-top: 50px;
+    width: 100%;
+    height: 300px;
+    border: none;
+    border-radius: 10px;
+
+`
+
+export const Table = styled.table``
+
+export const Row = styled.tr``
+
+export const Column = styled.td``
